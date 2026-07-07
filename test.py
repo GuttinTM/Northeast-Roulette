5f039954e8c68d1c7d98a060f3f190266653ba22
Guttin <levysoarescrato2020@gmail.com>
2026-07-06 21:10:15 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
 # You can use this file to test whatever you want
-
-from random import sample
-lista1 = [True, True, True]
-lista2 = [False, False, False]
-lista3 = sample(lista1 + lista2, k=len(lista1 + lista2))
-print(lista3)
