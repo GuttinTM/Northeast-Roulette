b78e114f2e281beb18ecd62e62fa3d29b1b15abc
Guttin <levysoarescrato2020@gmail.com>
2026-07-06 21:05:58 -0300

    Refactor gen_order_bullets function parameters
    Refactor gen_order_bullets function to accept a number parameter instead of using the length of list_bullet.

--- generation_orders_bullets.py
+++ generation_orders_bullets.py
@@ -1,12 +1,11 @@
-def gen_order_bullets(list_bullet, valor):
-    list_bullet = list()
-    
-    if valor == True:
-        for bullet in range(0, len(list_bullet)):
+def gen_order_bullets(list_bullet, bool, numb ):
+
+    if bool == True:
+        for bullet in range(0, numb):
             list_bullet.append(True)
         return list_bullet
     
     else:
-        for bullet in range(0, len(list_bullet)):
+        for bullet in range(0, numb):
             list_bullet.append(False)
         return list_bullet
