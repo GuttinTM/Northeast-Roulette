68527b40a96765732574c8b8caa0f1f93c902e68
Guttin_TM <levysoarescrato2020@gmail.com>
2026-07-09 01:19:25 -0300

    Now the function title() requires four parameters (for now, maybe that will change): word1, word2, color1 and color2).

--- title.py
+++ title.py
@@ -1,7 +1,12 @@
-def title(msg):
-    print('-' * 30)
-    print(f'{msg}'.center(30))
-    print('-' * 30)
+from rich.console import Console
+console = Console()
+
+def title(word1, word2, color, color2):
+    console.print(f"[{color}]-" * 148)
+    console.print(f"[{color}]{word1} [{color2}]{word2}", justify="center")
+    console.print(f"[{color2}]-" * 148)
 
 def line(num):
     print('-' * num)
+
+
