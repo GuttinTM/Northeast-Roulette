868613f813df1df461ae360aefec07ab8649ee93
Guttin_TM <levysoarescrato2020@gmail.com>
2026-07-09 01:33:58 -0300

    The new feature updated

--- title.py
+++ title.py
@@ -1,10 +1,10 @@
 from rich.console import Console
-console = Console()
+console = Console(width=100)
 
 def title(word1, word2, color, color2):
-    console.print(f"[{color}]-" * 148)
-    console.print(f"[{color}]{word1} [{color2}]{word2}", justify="center")
-    console.print(f"[{color2}]-" * 148)
+    console.print(f"[{color}]-" * 100)
+    console.print(f"[{color}]{word1} [{color2}]{word2}", justify='center')
+    console.print(f"[{color2}]-" * 100)
 
 def line(num):
     print('-' * num)
