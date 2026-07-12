ff672b5238468999ca2df0dad18aba4c76eef402
Guttin_TM <levysoarescrato2020@gmail.com>
2026-07-12 16:59:09 -0300

    Title updated

--- title.py
+++ title.py
@@ -1,7 +1,7 @@
 from rich.console import Console
 console = Console(width=100)
 
-def title(word1, word2, color, color2):
+def title(word1, color, word2="", color2=""):
     console.print(f"[{color}]-" * 100)
     console.print(f"[{color}]{word1} [{color2}]{word2}", justify='center')
     console.print(f"[{color2}]-" * 100)
